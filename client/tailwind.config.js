24f8f75a0636ae9216b889f5f751cc26d03cd28e
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-12-05 23:57:16 +0300

    Configure the poppins font and the primary colors for the project

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: '#101010',
+        secondary: '#56A46E',
+        background: '#FFFFFF',
+      }
+    },
   },
   plugins: [],
 }
