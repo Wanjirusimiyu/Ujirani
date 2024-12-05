28d312947a76902ef58d59811d12d6e5ca123701
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-12-05 23:25:45 +0300

    Edit the App.js file to test the tailwind configurations

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
+
 function App() {
-  return <h1>Project Client</h1>;
+  return <h1 className="text-3xl font-bold underline">Project Client</h1>;
 }
 
 export default App;
