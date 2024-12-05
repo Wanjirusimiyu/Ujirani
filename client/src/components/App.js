760f76ac798e3089cd8009c8a5973ba7c00dec04
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-12-06 01:08:36 +0300

    Import the signup page from the pages folder

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import SignUp from "../pages/SignUp";
 
 
 function App() {
-  return <h1 className="font-poppins text-3xl font-bold underline">Project Client</h1>;
+  return <>
+      <SignUp />
+  </>;
 }
 
 export default App;
