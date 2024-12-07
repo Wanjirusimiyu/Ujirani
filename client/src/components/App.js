73f77097d37b70e7ea58995e6cbda2de2a42e46e
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-12-07 16:38:11 +0300

    Add the routes for the signup and loginpage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
 
-
 function App() {
-  return <>
-      <SignUp />
-      <Login />
-  </>;
+  return (
+    <Router>
+      <Routes>
+        {/* Define the Login route */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Define the SignUp route */}
+        <Route path="/signup" element={<SignUp />} />
+
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
