5e62b9e2c8240cd981906788de770c59859deab1
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-12-07 11:35:25 +0300

    Set up the logo section and footer section

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import ujirani from '../assets/ujirani.png'
 
 const Login = () => {
   return (
-    <div>
+    // Main container
+    <div className='relative font-poppins min-h-screen max-h-screen bg-black flex'>
+        {/* Left logo Section */}
+        <div className='w-1/3 text-white flex flex-col justify-center items-center p-10'>
+        <img 
+          src={ujirani}
+          alt="Ujirani Logo"
+          className='h-auto w-auto -mt-24 mb-6'
+        />
+        <p className='text-sm font-semibold text-center -mt-28 mb-28'>
+            Connect with your neighbors!
+        </p>
+        {/* footer Section */}
+        <footer className="mt-10 text-sm text-gray-500 text-center -mb-24 justify-center items-center ">
+          <p className="mb-6">Help · Guidelines · Legal Policy</p>
+          <p className="mb-6">About · Press · Blog</p>
+          <p className="text-white">© 2024 ITR Business Solutions</p>
+        </footer>
+        </div>
       
     </div>
   )
