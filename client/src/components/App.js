f60b1e9ea03cb7fd45eab814f32f139fe479018d
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-12-07 11:20:48 +0300

    Modify the App.js file by importing the Login.js file in the pages folder

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import SignUp from "../pages/SignUp";
+import Login from "../pages/Login";
 
 
 function App() {
   return <>
       <SignUp />
+      <Login />
   </>;
 }
 
