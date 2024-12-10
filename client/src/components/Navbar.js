dd512d577e264a690cdf27020198216c63e1f73d
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-12-10 21:01:39 +0300

    Create a Navbar.js file in the components folder with a basic structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import Navbar from './Navbar';
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="bg-white shadow p-4 rounded-3xl" >
+    <header className="bg-white shadow p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">Ujirani</h1>
+        <input
+          type="text"
+          placeholder="Search here"
+          className="border rounded px-4 py-2 w-1/3"
+        />
+      </div>
+    </header>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
