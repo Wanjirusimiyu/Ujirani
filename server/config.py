8c8966044a2a4b58bc1d4df4de65d5a403dc90b1
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-12-26 00:41:32 +0300

    Set up the file by modifying it its path to the database.db file

--- server/config.py
+++ server/config.py
@@ -1,31 +1,51 @@
-# Standard library imports
+# # Standard library imports
 
-# Remote library imports
-from flask import Flask
-from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+# # Remote library imports
+# from flask import Flask
+# from flask_cors import CORS
+# from flask_migrate import Migrate
+# from flask_restful import Api
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
+# import os
 
-# Local imports
+# # Local imports
 
-# Instantiate app, set attributes
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+# # Get the absolute path to the instance folder
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Define metadata, instantiate db
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-db = SQLAlchemy(metadata=metadata)
-migrate = Migrate(app, db)
-db.init_app(app)
 
-# Instantiate REST API
-api = Api(app)
+# # Specify the database file inside the instance folder
+# SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance', 'database.db')}"
 
-# Instantiate CORS
-CORS(app)
+# # Instantiate app, set attributes
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/app.db'
+
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = False
+
+# # Define metadata, instantiate db
+# metadata = MetaData(naming_convention={
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
+# db = SQLAlchemy(metadata=metadata)
+# migrate = Migrate(app, db)
+# db.init_app(app)
+
+# # Instantiate REST API
+# api = Api(app)
+
+# # Instantiate CORS
+# CORS(app)
+
+# config.py
+
+import os
+
+# Get the absolute path to the current file (i.e., the config.py file itself)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Specify the database file inside the instance folder
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance', 'database.db')}"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
