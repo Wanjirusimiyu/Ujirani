68e70be0dc7f645c7c7620ca704af4cce27b6d36
Joy Wanjiru <wanjirusimiyu@gmail.com>
2025-01-07 13:27:54 +0300

    edit the User Model to add the is verified row

--- server/models.py
+++ server/models.py
@@ -8,9 +8,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=True)
+    is_verified = db.Column(db.Boolean, default=False)
 
-    def __init__(self, fullname, email, password):
+    def __init__(self, fullname, email, password, is_verified=False):
         self.fullname = fullname
         self.email = email
         self.password = password
+        self.is_verified = is_verified
+
+    def __repr__(self):
+        return f'<User {self.fullname}>'
