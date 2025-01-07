a18f24304d26cae1eafac7aa4f5ba206a12497c8
Joy Wanjiru <wanjirusimiyu@gmail.com>
2025-01-07 13:21:08 +0300

    Set up google 0 auth Configuration in app component by wrapping the routes around O Auth provider by importing it from react O Auth library

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 
 function App() {
   return (
+    <GoogleOAuthProvider clientId="767607723850-hbaanr47si7aiqdtf4qa5h3a59cirpth.apps.googleusercontent.com">
     <Router>
       <Routes>
         {/* Define the Login route */}
@@ -18,6 +20,7 @@ function App() {
 
       </Routes>
     </Router>
+  </GoogleOAuthProvider>  
   );
 }
 
